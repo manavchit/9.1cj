a04d320d18731c88294ee5938acad2f505b4a1b7
manavchit <manav771177@gmail.com>
2025-02-12 13:41:59 +0530

    Update Welcome.js

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,13 +1,10 @@
 // Welcome.js
 import { useNavigate } from "react-router-dom";
-import { auth } from "./firebase"; // Ensure you have Firebase auth imported
-import { signOut } from "firebase/auth";
 
 function Welcome() {
   const navigate = useNavigate();
 
-  const handleSignOut = async () => {
-    await signOut(auth);
+  const handleSignOut = () => {
     navigate("/"); // Redirect to homepage after sign-out
   };
 
